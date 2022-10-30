9f0218f35f5f9fe0720844298e50eea3da95e44d
Amy <86963506+aeposten@users.noreply.github.com>
2022-10-30 15:35:50 -0700

    Initial algo for k most frequent written, need to figure out how to work with object

--- src/App.js
+++ src/App.js
@@ -1,4 +1,29 @@
+import listen_history from './data/listen_history.json'
 function App() {
+  const DATA = listen_history;
+
+  function mostFrequent (arr, k) {
+    const frequency = new Map();
+    const bucket = [];
+    const result = [];
+
+    for (let ele of arr) {
+      frequency.set(arr, (frequency.get(arr) || 0) + 1);
+    }
+
+    for (let [ele, freq] of frequency) {
+      bucket[freq] = (bucket[freq] || new Set().add(ele))
+    }
+
+    for (let i = bucket.length; i >= 0; i--) {
+      if (bucket[i]) result.push(...bucket[i]);
+      if (result.length >= k) break;
+    }
+    return result;
+  }
+
+  console.log(mostFrequent(DATA, 10))
+
   return (
     <div className="App">
       It's an app!.
