b3c13b10f3167ddc0ff9f9602b8b7112a107fd5f
Amy <86963506+aeposten@users.noreply.github.com>
2022-10-31 09:40:45 -0700

    Can get 7 most frequent days need to figure out how to grab day of week from year month date

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ function App() {
   const DATA = listen_history;
   let dateArr = [];
 
-
   function mostFrequent(arr, category, k) {
     const frequency = new Map();
     const bucket = [];
     const result = [];
 
+    if (category) {
+      category = category;
+    } else {
+      category = null;
+    }
 
     for (let ele of arr) {
-      frequency.set(ele[category], (frequency.get(ele[category]) || 0) + 1);
+      if (category) {
+        frequency.set(ele[category], (frequency.get(ele[category]) || 0) + 1);
+      } else {
+        frequency.set(ele, (frequency.get(ele) || 0) + 1);
+      }
     }
 
     for (let [ele, freq] of frequency) {
-      bucket[freq] = (bucket[freq] || new Set().add({ title: ele, frequency: freq }))
+      bucket[freq] =
+        bucket[freq] || new Set().add({ title: ele, frequency: freq });
     }
 
     for (let i = bucket.length; i >= 0; i--) {
@@ -30,16 +39,15 @@ function App() {
   }
 
   function getDate(arr) {
-
-
     for (let ele of arr) {
-      dateArr.push(ele.time.slice(0,10))
+      dateArr.push(ele.time.slice(0, 10));
     }
-    return dateArr
+    return dateArr;
   }
 
-  console.log(getDate(DATA))
+  getDate(DATA);
   console.log(mostFrequent(DATA, "title", 10));
+  console.log(mostFrequent(dateArr, null, 7));
 
   return <div className="App">It's an app!.</div>;
 }
